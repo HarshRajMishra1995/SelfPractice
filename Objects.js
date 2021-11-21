b4e2473f64f331e91900707a05b5cfa79f0d7d18
HarshRajMishra <HarshRajMishra.1995@gmail.com>
2021-11-21 11:20:51 +0530

    Object.js added

---
+++ Objects.js
@@ -0,0 +1,18 @@
+// var myObject=new Object();
+
+// myObject.name="Car";
+// myObject.model="mustang";
+// myObject.color="white";
+
+// console.log(myObject.name);
+
+// var newObject={
+//     color:"white",
+//     nameofcar:"mustang",
+//     place:"LA"
+// }
+// console.log(newObject.nameofcar)
+
+var myobj=new Object(),str='myString',rand=Math.random(),obj=new Object();
+
+console.log(typeof str)
